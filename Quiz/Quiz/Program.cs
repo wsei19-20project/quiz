13d789b567ac6008752125e6ee1e78d3ec6be737
arkadiusz971 <teh0lzon@gmail.com>
2020-01-11 21:41:43 +0100

    API with Questions added

--- Quiz/Quiz/Program.cs
+++ Quiz/Quiz/Program.cs
@@ -1,5 +1,6 @@
 ﻿using quizLib;
 using System;
+using System.Net;
 
 namespace Quiz
 {
@@ -7,7 +8,35 @@ namespace Quiz
     {
         static void Main(string[] args)
         {
+    
+            string apiUrl = "https://opentdb.com/api.php?amount=1&type=multiple";
+
+            var client = new WebClient();
+            var content = client.DownloadString(apiUrl);
+
+            var obj = Newtonsoft.Json.JsonConvert.DeserializeObject<Questions>(content);
+            Console.WriteLine(obj.results[0].question);
+
             QuizLib1.quizInfo();
+
         }
+
+    }
+
+    public class Results
+    {
+        public string category;
+        public string type;
+        public string difficulty;
+        public string question;
+        public string correct_answer;
+        public string[] incorrect_answer;
+    }
+
+    public class Questions
+    {
+        public int response_code;
+        public Results[] results;
+
     }
 }
