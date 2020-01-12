d3691b07ed213bcfca0321d6c866f41e8810dabc
C4mi110 <45486615+C4mi110@users.noreply.github.com>
2020-01-12 14:49:35 +0100

    WPF added with random answers

--- Quiz/Quiz/Program.cs
+++ Quiz/Quiz/Program.cs
@@ -8,19 +8,49 @@ namespace Quiz
     {
         static void Main(string[] args)
         {
-    
-            string apiUrl = "https://opentdb.com/api.php?amount=1&type=multiple";
+            QuizMenager q1 = new QuizMenager();
+            //var questions = q1.getQuestion();
+            //q1.playQuiz(questions);
+            var q = QuizLib1.generateQuestion();
 
-            var client = new WebClient();
-            var content = client.DownloadString(apiUrl);
+            string[] answersArray = new string[4] { q.results[0].correct_answer, q.results[0].incorrect_answers[0], q.results[0].incorrect_answers[1], q.results[0].incorrect_answers[2] };
+            int index;
+            string tmp;
+            Random rand = new Random();
 
-            var obj = Newtonsoft.Json.JsonConvert.DeserializeObject<Questions>(content);
-            Console.WriteLine(obj.results[0].question);
+            for (int i = 0; i < answersArray.Length; i++)
+            {
+                index = rand.Next(answersArray.Length);
 
-            QuizLib1.quizInfo();
+                tmp = answersArray[i];
+                answersArray[i] = answersArray[index];
+                answersArray[index] = tmp;
+            }
+
+            foreach (var a in answersArray)
+                Console.WriteLine(a);
+
+            Console.WriteLine("C: " + q.results[0].correct_answer);
+            //QuizLib1.quizInfo();
 
         }
 
+    }
+    public class QuizMenager
+    {
+        
+        public void playQuiz(Questions q)
+        {
+            var result = q.results[0];
+
+            Console.WriteLine("Question: " + result.question);
+
+            Console.WriteLine("C: " + result.correct_answer);
+            for (int i = 0; i < result.incorrect_answers.Length; i++)
+            {
+                Console.WriteLine("F: " + result.incorrect_answers[i]);
+            }
+        }
     }
 
     public class Results
@@ -30,7 +60,7 @@ namespace Quiz
         public string difficulty;
         public string question;
         public string correct_answer;
-        public string[] incorrect_answer;
+        public string[] incorrect_answers;
     }
 
     public class Questions
--- Quiz/quizLib/quiz.cs
+++ Quiz/quizLib/quiz.cs
@@ -1,12 +1,70 @@
 ﻿using System;
+using System.Net;
 
 namespace quizLib
 {
     public class QuizLib1
     {
+        public class Results
+        {
+            public string category;
+            public string type;
+            public string difficulty;
+            public string question;
+            public string correct_answer;
+            public string[] incorrect_answers;
+        }
+
+        public class Questions
+        {
+            public int response_code;
+            public Results[] results;
+
+        }
+
         public static void quizInfo()
         {
             Console.WriteLine("Quiz Liblary");
-        } 
+        }
+
+        public static Questions generateQuestion()
+        {
+            string apiUrl = "https://opentdb.com/api.php?amount=1&type=multiple";
+            var client = new WebClient();
+            var content = client.DownloadString(apiUrl);
+
+            var obj = Newtonsoft.Json.JsonConvert.DeserializeObject<Questions>(content);
+            return obj;
+        }
+
+        public static string getQuestion(Questions q)
+        {
+            return q.results[0].question;
+        }
+
+        public static string getCorrectAnswer(Questions q)
+        {
+            return q.results[0].correct_answer;
+        }
+
+        public static string[] getAnswers(Questions q)
+        {
+            string[] answersArray = new string[4] {q.results[0].correct_answer, q.results[0].incorrect_answers[0], q.results[0].incorrect_answers[1], q.results[0].incorrect_answers[2]};
+            int index;
+            string tmp;
+            Random rand = new Random();
+
+            for (int i = 0; i < answersArray.Length; i++)
+            {
+                index = rand.Next(answersArray.Length);
+
+                tmp = answersArray[i];
+                answersArray[i] = answersArray[index];
+                answersArray[index] = tmp;
+            }
+
+            return answersArray;
+        }
+
     }
 }
